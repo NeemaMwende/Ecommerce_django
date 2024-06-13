72f1282326ca8339a1ee82f3a17298e8ea0242bb
NeemaMwende <gracepete12@gmail.com>
2024-06-13 14:11:37 +0300

    updates

--- store/models.py
+++ store/models.py
@@ -8,17 +8,24 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+    #Customers
 class Customer(models.Model):
-    first_name =
-    last_name =
-    phone =
-    email =
-    password =
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
     
 
+#all of our products
 class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
 
 class Order(models.Model):
