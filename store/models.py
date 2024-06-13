69cdaa7e0dc62fb6ba421d6e0fc13dfab73c9ac1
NeemaMwende <gracepete12@gmail.com>
2024-06-13 13:05:03 +0300

    models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Customer(models.Model):
+    first_name =
+    last_name =
+    phone =
+    email =
+    password =
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+
+class Product(models.Model):
+
+class Order(models.Model):
