69de642a5ca9137aa0ed64ea288165defc126b79
NeemaMwende <gracepete12@gmail.com>
2024-06-13 16:49:10 +0300

    models

--- store/models.py
+++ store/models.py
@@ -23,9 +23,24 @@ class Customer(models.Model):
 
 #all of our products
 class Product(models.Model):
-    name = models.CharField(max_length=50)
-    price = models.CharField(max_length=50)
-    category = models.CharField(max_length=50)
-    description = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
 
-# class Order(models.Model):
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer =models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', blank=True)
+    phone = models.CharField(max_length=20, default='', blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product
+    
